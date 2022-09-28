29d84069270fdd20231c975533ad3e5019e67318
FarazBagwan <farazbagwan11@gmail.com>
2022-09-28 16:14:22 +0530

    Commit-27

--- test2.rb
+++ test2.rb
@@ -17,6 +17,6 @@ describe file('C:/Windows/explorer.exe') do
     it { should exist }
     it { should be_disabled }
     its('logon_mode') { should eq 'Interactive/Background' }
-    its('last_result') { should cmp 267011 }
+   # its('last_result') { should cmp 267011 }
     its('task_to_run') { should cmp '%Windir%\system32\appidpolicyconverter.exe' }
   end
